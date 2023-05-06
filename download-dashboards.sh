e9614131b29de2713063cd0c614b16e71c1b892c
Gonçalo MB <me@goncalomb.com>
2023-05-06 19:41:08 +0100

    Enable traefik metrics

--- download-dashboards.sh
+++ download-dashboards.sh
@@ -5,6 +5,9 @@ cd -- "$(dirname -- "$0")"
 
 DASHBOARDS=(
     1860-31 # https://grafana.com/grafana/dashboards/1860-node-exporter-full/
+    17346-7 # https://grafana.com/grafana/dashboards/17346-traefik-official-standalone-dashboard/
+    4475-5 # https://grafana.com/grafana/dashboards/4475-traefik/
+    11462-1 # https://grafana.com/grafana/dashboards/11462-traefik-2/
 )
 
 ./web-conductor.py compose -- exec grafana mkdir -p /var/lib/grafana/dashboards
@@ -13,4 +16,6 @@ for V in ${DASHBOARDS[*]}; do
     ./web-conductor.py compose -- exec grafana \
         wget -O "/var/lib/grafana/dashboards/$V.json" \
         "https://grafana.com/api/dashboards/${V%-*}/revisions/${V##*-}/download"
+    # https://github.com/grafana/grafana/issues/10786
+    ./web-conductor.py compose -- exec grafana sed -i "s/\${DS_PROMETHEUS}//g" "/var/lib/grafana/dashboards/$V.json"
 done
